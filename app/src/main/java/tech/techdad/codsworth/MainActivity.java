436865d77f7b51a51e49fe56142a441869fdf859
Andy Brown <andy@techdad.tech>
2018-12-20 19:14:40 +0000

    changing icons to use ones with a 'theme'

--- app/src/main/java/tech/techdad/codsworth/MainActivity.java
+++ app/src/main/java/tech/techdad/codsworth/MainActivity.java
@@ -17,14 +17,14 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                case R.id.navigation_lists:
+                    mTextMessage.setText(R.string.title_lists);
                     return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                case R.id.navigation_reference:
+                    mTextMessage.setText(R.string.title_reference);
                     return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                case R.id.navigation_settings:
+                    mTextMessage.setText(R.string.title_settings);
                     return true;
             }
             return false;
